4c7763fab55be53d6d44a8c01b089afaca4da561
Sebastian Shinnick <sebastianshinnick@gmail.com>
2025-03-27 17:01:36 -0400

    Fixed command problem with auto and cleaned it up more
    You can't use a command for both a command group and a controller scheduler

--- src/robotcontainer.py
+++ src/robotcontainer.py
@@ -105,7 +105,6 @@ class RobotContainer:
         self.drivetrain = TunerConstants.create_drivetrain()
 
         self.vision = VisionSubsystem(self.drivetrain, "limelight-four")
-        #self.auto_align = AutoAlign(self.drivetrain, self.vision)
         # NOTE: HAVE ALL THE ENABLY THINGS HERE (and change them all to true when actually playing)
 
         self.ENABLE_ALGAE = True
@@ -120,23 +119,11 @@ class RobotContainer:
         if self.ENABLE_ALGAE:
             self.algaeSubsystem = algaeSubsystem.AlgaeSubsystem()
 
-
         if self.ENABLE_ELEVATOR:
             self.elevatorSubsystem = elevatorSubsystem.ElevatorSubsystem()
 
         if self.ENABLE_CORAL:
             self.coralSubsystem = coralSubsystem.CoralTrack()
-            self.dischargeCoralRightCommand = coralCommands.DischargeCoralCommand(
-                self.coralSubsystem,
-                # self.elevatorSubsystem,
-                direction = -1,  
-            )
-
-            self.dischargeCoralLeftCommand = coralCommands.DischargeCoralCommand(
-                self.coralSubsystem,
-                # self.elevatorSubsystem,
-                direction = 1, 
-            )
 
         if self.ENABLE_CLIMB:
             self.climbSubsystem = climbSubsystem.ClimbSubsystem()
@@ -146,7 +133,6 @@ class RobotContainer:
 
         if self.ENABLE_VISON:
             self.vision = vison.VisionSubsystem(self.drivetrain)
-            self.auto_align = PathOnTheFlyAutoAlign(self.drivetrain, self.vision, False)
 
         # Configure the button bindings
         self.configureButtonBindings()
@@ -159,14 +145,40 @@ class RobotContainer:
             ))
 
         # Path follower
-        NamedCommands.registerCommand("dischargeCoralLeftCommand", self.dischargeCoralLeftCommand)
-        NamedCommands.registerCommand("dischargeCoralRightCommand", self.dischargeCoralRightCommand)
-        NamedCommands.registerCommand("AutoAlign", self.auto_align)
-
+        NamedCommands.registerCommand("dischargeCoralLeftCommand", 
+            coralCommands.DischargeCoralCommand(
+                self.coralSubsystem,
+                # self.elevatorSubsystem,
+                direction = -1,  
+            )
+        )
+        NamedCommands.registerCommand("dischargeCoralRightCommand", 
+            coralCommands.DischargeCoralCommand(
+                self.coralSubsystem,
+                # self.elevatorSubsystem,
+                direction = 1, 
+            )
+        )
+        NamedCommands.registerCommand("RightAutoAlign", 
+            PathOnTheFlyAutoAlign(
+                self.drivetrain, 
+                self.vision, 
+                False
+            )
+        )
+        NamedCommands.registerCommand("LeftAutoAlign", 
+            PathOnTheFlyAutoAlign(
+                self.drivetrain, 
+                self.vision, 
+                True
+            )
+        )
 
         #After registering named commands, we can build the auto chooser
         self._auto_chooser = AutoBuilder.buildAutoChooser()
         SmartDashboard.putData("Auto Mode", self._auto_chooser)
+
+        commands2.SequentialCommandGroup
 
     def getAutonomousCommand(self):
         return self._auto_chooser.getSelected()
@@ -301,7 +313,18 @@ class RobotContainer:
         if self.ENABLE_CORAL:
             # Declare Coral Sequential Commands
             defaultCoralCommand = coralCommands.CoralDefaultCommand(self.coralSubsystem)
+            
+            self.dischargeCoralRightCommand = coralCommands.DischargeCoralCommand(
+                self.coralSubsystem,
+                # self.elevatorSubsystem,
+                direction = -1,  
+            )
 
+            self.dischargeCoralLeftCommand = coralCommands.DischargeCoralCommand(
+                self.coralSubsystem,
+                # self.elevatorSubsystem,
+                direction = 1, 
+            )
             
             # 0.53 0.27
             self.coralSubsystem.setDefaultCommand(defaultCoralCommand)
